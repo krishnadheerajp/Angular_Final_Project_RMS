bae8b58b1e233b6ca4ada5ea4b3aac3b71b44a46
Krishna Dheeraj <krishnadheeraj@L-H3Q9134XF3.local>
2023-02-14 22:23:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EventsComponent } from './events/events.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+MenuComponent,
+EventsComponent,
+HomeComponent,
+AboutComponent,
+GalleryComponent,
+ContactComponent
+],
+imports: [
+BrowserModule,
+routes,
+FormsModule,
+ReactiveFormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
